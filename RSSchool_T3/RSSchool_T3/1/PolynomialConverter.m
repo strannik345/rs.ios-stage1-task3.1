638db07aa7afefc8cee7d14c155bc68596d2f6b8
Volodko <volodkovasya@gmail.com>
2020-04-06 10:41:41 -0700

    feat: add 1 task

--- RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
+++ RSSchool_T3/RSSchool_T3/1/PolynomialConverter.m
@@ -2,6 +2,48 @@
 
 @implementation PolynomialConverter
 - (NSString*)convertToStringFrom:(NSArray <NSNumber*>*)numbers {
-    return @"";
+    NSMutableString *answer = [[NSMutableString alloc] init];
+    int max = numbers.count-1;
+    if(numbers.count == 0) return nil;
+    for (int i =0; i<numbers.count; i++) {
+        if([[numbers objectAtIndex:i] intValue]==0){
+            max--;
+            continue;
+        }
+        else{
+            if([[numbers objectAtIndex:i] intValue] >0){
+                if (i!=0) {
+                    [answer appendString:@"+ "];
+                }
+                if([[numbers objectAtIndex:i] intValue]!=1){
+                    [answer appendString:[[numbers objectAtIndex:i] stringValue]];
+                }
+            }
+            else{
+                if ([[numbers objectAtIndex:i] intValue]==-1) {
+                    [answer appendString:@"- "];
+                }
+                else{
+                    [answer appendString:@"- "];
+                    [answer appendString:[NSString stringWithFormat:@"%d",[[numbers objectAtIndex:i] intValue]*(-1)]];
+                }
+            }
+            if(max!=0){
+                if(max==1){
+                    [answer appendString:@"x"];
+                }
+                else{
+                    [answer appendString:@"x^"];
+                    [answer appendString:[NSString stringWithFormat:@"%d",max]];
+                }
+                max--;
+                [answer appendString:@" "];
+            }
+            
+        }
+    }
+    if([answer characterAtIndex:answer.length-1]==' ') [answer deleteCharactersInRange:NSMakeRange([answer length]-1, 1)];
+        
+    return answer;
 }
 @end
